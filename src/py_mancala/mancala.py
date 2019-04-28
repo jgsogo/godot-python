d9dd33bc29f785592e5d7d8340a3fa859b9b6ed2
Javier G. Sogo <jgsogo@gmail.com>
2019-04-28 21:11:32 +0200

    add state to python code

--- src/py_mancala/mancala.py
+++ src/py_mancala/mancala.py
@@ -2,10 +2,17 @@
 class Mancala:
     def __init__(self):
         print("(python) Mancala::init")
+        self.state = 0
 
     def get_state(self):
         print("(python) Mancala::get_state")
-        return "(python) current state"
+        self.state += 1
+        return "(python) current state: {!r}".format(self.state)
 
     def play_position(self, value):
         print("(python) Mancala::play_position(value={!r})".format(value))
+        self.state = value
+
+
+# Create a single object to keep state
+mancala = Mancala()
