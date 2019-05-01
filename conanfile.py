976ceb672cc7ac9d1b64d0a8a903269ffaebd689
Javier G. Sogo <jgsogo@gmail.com>
2019-05-01 18:06:51 +0200

    use python from Conan

--- conanfile.py
+++ conanfile.py
@@ -10,6 +10,7 @@ class GodotPython(ConanFile):
     def requirements(self):
         self.requires("pybind11/2.2.4@conan/stable")
         self.requires("godot-cpp/[~0.0.0-0,include_prerelease=True]@jgsogo/stable")
+        self.requires("cpython/3.7.3@jgsogo/stable")
 
     def build(self):
         cmake = CMake(self)
